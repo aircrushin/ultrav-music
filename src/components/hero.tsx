567bc703d95a680db9a3847b22a11e40da64e3a9
UltraV <1216564567@qq.com>
2024-08-21 22:50:58 +0800

    add banner

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -10,21 +10,21 @@ export default function Hero() {
           <div className="flex flex-col items-center space-y-8 text-center">
             <div className="space-y-6">
               <h1 className="leading-[60px] text-5xl sm:text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter animate-scale-in lg:max-w-5xl max-w-2xl">
-                Immerse Yourself in the Sounds of <span className='text-gradient'>UltraV</span>
+                the Sounds of <span className='text-gradient'>UltraV</span>
               </h1>
-              <p className="mx-auto max-w-[700px] text-gray-300 md:text-xl animate-fade-in">
-                <Typewriter text="Discover the captivating Tropical House music of UltraV" speed={30} />
+              <p className="mx-auto max-w-[1000px] text-gray-300 md:text-xl animate-fade-in">
+                <Typewriter text="Music Producer/DJ -- Tropical House -- Chill Vibes -- This is how tomorrow moves" speed={30} />
               </p>
             </div>
             <div className="mt-8 flex space-x-8">
               <Link href="https://music.163.com/#/artist?app_version=9.0.80&id=12120673&dlt=0846" target="_blank">
-                <button className="inline-flex h-12 items-center justify-center rounded-md bg-purple-700 px-6 py-3 text-sm font-medium text-gray-50 shadow-sm transition-colors hover:bg-purple-800/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300">Listen Now
+                <button className="inline-flex h-12 items-center justify-center rounded-md bg-purple-700 px-6 py-3 text-sm font-medium text-gray-50 shadow-sm transition-colors hover:bg-purple-800/90 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:bg-gray-50 dark:text-gray-900 dark:hover:bg-gray-50/90 dark:focus-visible:ring-gray-300 w-full md:w-auto">Listen Now
                 </button>
               </Link>
-              <Link href="/#music-section">
-                <button className="inline-flex h-12 items-center justify-center rounded-md border text-gray-900 bg-white px-6 py-3 text-sm font-medium shadow-sm transition-colors hover:bg-gray-300 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300">Learn More
+              {/* <Link href="/#music-section">
+                <button className="inline-flex h-12 items-center justify-center rounded-md border text-gray-900 bg-white px-6 py-3 text-sm font-medium shadow-sm transition-colors hover:bg-gray-300 hover:text-gray-900 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-gray-950 disabled:pointer-events-none disabled:opacity-50 dark:border-gray-800 dark:bg-gray-950 dark:hover:bg-gray-800 dark:hover:text-gray-50 dark:focus-visible:ring-gray-300 w-full md:w-auto">Learn More
                 </button>
-              </Link>
+              </Link> */}
             </div>
           </div>
         </div>
