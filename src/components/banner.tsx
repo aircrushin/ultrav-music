ee5afa90cebfcd2f1e7f0af3cae852f6c5e04dfb
UltraV <1216564567@qq.com>
2024-08-23 14:21:19 +0800

    add human

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -3,9 +3,9 @@ export default () => {
         <div className="bg-gray-800 z-100">
             <div className="max-w-screen-xl mx-auto px-4 py-3 text-white sm:text-center md:px-8">
                 <p className="font-medium">
-                   "human" will be out on August 23rd 
-                   <a href="http://atla.st/mbhuman" className="font-semibold underline duration-150 hover:text-indigo-100 inline-flex items-center gap-x-2">
-                      <span className="px-1">Presave</span>  
+                   "human" is out now!
+                   <a href="https://open.spotify.com/track/0sDWUnDav1NkpyU0ytXhrH?si=3d9ef2ddc3674f08" className="font-semibold underline duration-150 hover:text-indigo-100 inline-flex items-center gap-x-2">
+                      <span className="px-1">Listen</span>  
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5">
                             <path fillRule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clipRule="evenodd" />
                         </svg>
